b054c41447911d7502c1c05b15cb710f270fffcb
Phiriyakorn Maneekongrit <phiriyakornman@gmail.com>
2023-02-21 11:00:21 +0700

    edit 5

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  // graphqlUri: 'http://13.250.41.39:8082/graphql'
-  graphqlUri: 'http://52.91.235.53:8085/graphql'
+  graphqlUri: 'http://13.250.41.39:8082/graphql'
+  // graphqlUri: 'http://52.91.235.53:8085/graphql'
 };
